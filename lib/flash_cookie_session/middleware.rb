de68c00f5724910ea6f3af1e3b2443442b4f6a36
Indrek Juhkam <indrek@urgas.eu>
2012-01-24 17:33:24 +0200

    Remove nils from the token array before join

--- lib/flash_cookie_session/middleware.rb
+++ lib/flash_cookie_session/middleware.rb
@@ -10,7 +10,7 @@ module FlashCookieSession
         req = Rack::Request.new(env)
         the_session_key = [ @session_key, req.params[@session_key] ].join('=').freeze if req.params[@session_key]
         the_remember_token = [ 'remember_token', req.params['remember_token'] ].join('=').freeze if req.params['remember_token']
-        cookie_with_remember_token_and_session_key = [ the_remember_token, the_session_key ].join(';').freeze
+        cookie_with_remember_token_and_session_key = [ the_remember_token, the_session_key ].compact.join(';').freeze
         env['HTTP_COOKIE'] = cookie_with_remember_token_and_session_key
         env['HTTP_ACCEPT'] = "#{req.params['_http_accept']}".freeze if req.params['_http_accept']
       end
